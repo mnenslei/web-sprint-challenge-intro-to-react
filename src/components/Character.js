85d0cd144402fbb745ad3d33f80167ef21f75864
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2021-11-05 15:39:58 -0400

    working on styling now

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from "react";
+
+const Character = (props) => {
+    const { name } = props
+
+    return (
+        <p>
+            {name}
+        </p>
+    )
+
+}
+
+export default Character;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { ThemeProvider } from "styled-components";
+
 import { worker } from "./mocks/browser";
 worker.start();
 
